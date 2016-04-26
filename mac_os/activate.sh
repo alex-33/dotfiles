f05e9a53dcd85114282468aefb6786264b1f9bf0
Alexey Dral <aadral@gmail.com>
2016-04-27 00:13:14 +0100

    show full paths in Finder (Mac OS)

--- mac_os/activate.sh
+++ mac_os/activate.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# show full pathes in Finder
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
+
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew cask install google-drive
--- mac_os/deactivate.sh
+++ mac_os/deactivate.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# show full pathes in Finder
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool NO
+
 # uninstall brew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
