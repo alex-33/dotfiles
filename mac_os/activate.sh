921802f962fc7da5f65d6a9e6ec9c57e4622603c
Alexey Dral <aadral@gmail.com>
2016-04-28 01:13:23 +0100

    trying to build mxnet (on Mac OS)

--- mac_os/activate.sh
+++ mac_os/activate.sh
@@ -6,5 +6,10 @@ defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install coreutils
+brew install pkg-config
+brew install htop
 brew cask install google-drive
 brew cask install transmit
+
+brew tap homebrew/versions
+brew install gcc49 --enable-cxx
--- science.sh
+++ science.sh
@@ -3,3 +3,10 @@
 brew tap homebrew/science
 brew install opencv
 brew install python
+
+git submodule update --init --recursive
+
+cd science/mxnet
+cp make/osx.mk ./config.mk
+make -j4
+cd -
