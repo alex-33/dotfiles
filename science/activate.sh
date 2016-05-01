0a9a727528d59059becac0d3b4a725dc83df5c0c
Alexey Dral <aadral@gmail.com>
2016-05-01 15:13:46 +0300

    install science R packages

--- science/activate.sh
+++ science/activate.sh
@@ -7,6 +7,27 @@ science_folder=$initial_folder/$(dirname $filepath)
 brew tap homebrew/science
 brew install R
 brew cask install rstudio
+# install R packages
+function install_R_package {
+    if [[ ! -z $2 ]]
+    then
+        _repo="$2"
+    else
+        _repo="http://cran.rstudio.com/"
+    fi
+    R -e "install.packages(\"$1\", repos=\"$_repo\")"
+}
+install_R_package ROCR
+install_R_package ISLR
+## generalized (non-)linear models
+install_R_package leaps
+install_R_package gam
+install_R_package glmnet
+install_R_package e1071
+## trees
+install_R_package tree
+install_R_package randomForest
+install_R_package gbm
 
 brew install opencv
 brew install python
