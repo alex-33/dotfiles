481e8a6ef985ec87a70cfb6c81b7cdac968c91c2
Alexey Dral <aadral@bigdatateam.org>
2019-06-03 00:16:38 +0300

    experiment with jupyterlab extensions

--- science/activate_conda.sh
+++ science/activate_conda.sh
@@ -8,3 +8,6 @@ conda install --file conda_requirements.txt
 
 conda install -c conda-forge nodejs
 jupyter labextension install @jupyterlab/toc
+jupyter labextension install jupyterlab_vim
+jupyter labextension install @lckr/jupyterlab_variableinspector
+jupyter labextension install @ryantam626/jupyterlab_code_formatter
