1d1781b1aa8a32da9d7afd557cbcac2c93cf4a42
Alexey Dral <aadral@bigdatateam.org>
2022-09-10 07:58:07 +0600

    setup homebrew env

--- mac_os/activate.sh
+++ mac_os/activate.sh
@@ -8,6 +8,9 @@ defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
 
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/aadral/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 brew install coreutils
 brew install pkg-config
 brew install vim
